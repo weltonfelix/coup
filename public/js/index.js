29780f78eeebb321c2ce9b4d2020a4782973f860
jlsm2 <joaolds.marrocos@gmail.com>
2025-02-25 12:16:43 -0300

    Mostrar as moedas dos oponentes

--- public/js/index.js
+++ public/js/index.js
@@ -48,9 +48,9 @@ socket.on('connect', () => {
     '/moedas': () => {
       const players = game.state.players;
       let message = '';
-      message += `Você: ${players.myPlayerId.coins} moedas.\n`
+      message += `Você: ${players[myPlayerId].coins} moedas.\n`
       for (const [playerId, player] of Object.entries(players)) {
-        if (playerId != myPlayerId) {
+        if (playerId !== myPlayerId) {
           message += `${player.name}: ${player.coins} moedas.\n`
         }
       }
