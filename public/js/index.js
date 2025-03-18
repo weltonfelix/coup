206d5b96817d37adeb008f00efddd8df5527ebbf
Welton Felix <wplf@cin.ufpe.br>
2025-03-18 18:20:06 +0000

    Adiciona histórico de comandos

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,29 @@
-import { Game } from './game.js';
-import { Renderer } from './renderer.js';
+import { Game } from "./game.js";
+import { Renderer } from "./renderer.js";
 
-const formElement = document.getElementById('form');
-const inputElement = document.getElementById('input');
-const messagesElement = document.getElementById('messages');
+const formElement = document.getElementById("form");
+const inputElement = document.getElementById("input");
+const messagesElement = document.getElementById("messages");
 
 const renderer = new Renderer(messagesElement);
 const game = new Game();
 
-let playerName = window.prompt('Qual o seu nome?');
+const commandHistory = [];
+let historyCursor = 0;
+const updateHistory = (command) => {
+  if (command !== commandHistory.at(-1)) {
+    commandHistory.push(command);
+    historyCursor = commandHistory.length - 1;
+  }
+};
+
+let playerName = window.prompt("Qual o seu nome?");
 if (!playerName) {
   renderer.renderReceivedMessage(
-    { name: 'JOGO' },
-    'Você precisa de um nome para jogar. Recarregue a página e tente novamente.'
+    { name: "JOGO" },
+    "Você precisa de um nome para jogar. Recarregue a página e tente novamente."
   );
-  throw new Error('No name provided');
+  throw new Error("No name provided");
 }
 
 const socket = io({
@@ -25,33 +34,33 @@ const socket = io({
 
 function sendTextMessage(message) {
   const el = renderer.renderSentMessage(message);
-  socket.emit('sendMessage', message, () => {
-    el.classList.remove('message-loading');
-    el.classList.add('message-sent');
+  socket.emit("sendMessage", message, () => {
+    el.classList.remove("message-loading");
+    el.classList.add("message-sent");
   }); // Essa função é chamada quando o servidor confirma o recebimento da mensagem
 }
 
 function renderSecretMessage(message) {
-  renderer.renderReceivedMessage({ name: 'JOGO (secreto)' }, message);
+  renderer.renderReceivedMessage({ name: "JOGO (secreto)" }, message);
 }
 
 const commands = {
-  '/iniciar': () => {
-    socket.emit('startGame');
+  "/iniciar": () => {
+    socket.emit("startGame");
   },
-  '/parar': () => {
-    socket.emit('stopGame');
+  "/parar": () => {
+    socket.emit("stopGame");
   },
 };
 
-socket.on('connect', () => {
+socket.on("connect", () => {
   const myPlayerId = socket.id;
   let myCards = [];
 
   const playerActions = {
-    '/moedas': () => {
+    "/moedas": () => {
       const players = game.state.players;
-      let message = '';
+      let message = "";
       message += `Você: ${players[myPlayerId].coins} moedas. <br/>`;
       for (const [playerId, player] of Object.entries(players)) {
         if (playerId !== myPlayerId) {
@@ -60,92 +69,111 @@ socket.on('connect', () => {
       }
       renderSecretMessage(message);
     },
-    '/cartas': () => {
+    "/cartas": () => {
       renderSecretMessage(
-        `Suas cartas: ${myCards.map((card) => card.name).join(' e ')}.`
+        `Suas cartas: ${myCards.map((card) => card.name).join(" e ")}.`
       );
     },
   };
 
   const inTurnActions = {
-    '/pegar': (amount) => {
-      socket.emit('gameAction', { action: 'drawCoins', param: amount });
+    "/pegar": (amount) => {
+      socket.emit("gameAction", { action: "drawCoins", param: amount });
     },
-    '/pagar': (amount) => {
-      socket.emit('gameAction', { action: 'payCoins', param: amount });
+    "/pagar": (amount) => {
+      socket.emit("gameAction", { action: "payCoins", param: amount });
     },
-    '/dropassassinato': (cardName) => {
+    "/dropassassinato": (cardName) => {
       if (
         !myCards.find(
           (card) => card.name.toLowerCase() === cardName.toLowerCase()
         )
       ) {
-        return renderSecretMessage('Você não tem essa carta.');
+        return renderSecretMessage("Você não tem essa carta.");
       }
-      socket.emit('gameAction', { action: 'dropCardMurder', param: cardName });
+      socket.emit("gameAction", { action: "dropCardMurder", param: cardName });
       myCards.splice(
         myCards.findIndex((card) => card.name === cardName),
         1
       );
     },
-    '/dropgolpe': (cardName) => {
+    "/dropgolpe": (cardName) => {
       if (
         !myCards.find(
           (card) => card.name.toLowerCase() === cardName.toLowerCase()
         )
       ) {
-        return renderSecretMessage('Você não tem essa carta.');
+        return renderSecretMessage("Você não tem essa carta.");
       }
-      socket.emit('gameAction', { action: 'dropCardCoup', param: cardName });
+      socket.emit("gameAction", { action: "dropCardCoup", param: cardName });
       myCards.splice(
         myCards.findIndex((card) => card.name === cardName),
         1
       );
     },
-    '/trocar': (cardName) => {
+    "/trocar": (cardName) => {
       if (
         !myCards.find(
           (card) => card.name.toLowerCase() === cardName.toLowerCase()
         )
       ) {
-        return renderSecretMessage('Você não tem essa carta.');
+        return renderSecretMessage("Você não tem essa carta.");
       }
-      socket.emit('gameAction', { action: 'exchangeCard', param: cardName });
+      socket.emit("gameAction", { action: "exchangeCard", param: cardName });
       // Não precisa remover, pois vai receber as duas cartas de volta com um updateCards
     },
-    '/embaixador': () => {
-      socket.emit('gameAction', { action: 'ambassador'});
+    "/embaixador": () => {
+      socket.emit("gameAction", { action: "ambassador" });
     },
-    '/devolver': (cartas) => {
-      const cartas2 = cartas.map((carta) => carta.replaceAll(',', ''));
+    "/devolver": (cartas) => {
+      const cartas2 = cartas.map((carta) => carta.replaceAll(",", ""));
       const myCardsNames = [...myCards];
       for (const carta of cartas2) {
         if (
           !myCardsNames.splice(
-            myCards.findIndex((card) => card.name.toLowerCase() === carta.toLowerCase()),
-            1)
+            myCards.findIndex(
+              (card) => card.name.toLowerCase() === carta.toLowerCase()
+            ),
+            1
+          )
         ) {
           return renderSecretMessage(`Você não tem a carta ${carta}.`);
         }
       }
-      socket.emit('gameAction', { action: 'returnCards', param: cartas2 });
-    }
+      socket.emit("gameAction", { action: "returnCards", param: cartas2 });
+    },
   };
 
-  formElement.addEventListener('submit', (event) => {
+  inputElement.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+      if (historyCursor === -1) historyCursor = commandHistory.length - 1;
+      const lastCommand = commandHistory.at(historyCursor--);
+      if (lastCommand) {
+        inputElement.value = lastCommand;
+      }
+    } else if (event.key === "ArrowDown") {
+      if (historyCursor === commandHistory.length) historyCursor = 0;
+      const nextCommand = commandHistory.at(historyCursor++);
+      if (nextCommand) {
+        inputElement.value = nextCommand;
+      }
+    }
+  });
+
+  formElement.addEventListener("submit", (event) => {
     event.preventDefault();
     const message = inputElement.value;
     if (message.length === 0) return;
 
     // Check if the message is a game command
-    if (message.startsWith('/')) {
+    if (message.startsWith("/")) {
       console.log(game.state);
       if (!game.isPlayerInGame(myPlayerId)) {
         return renderSecretMessage(
-          'Você não está no jogo! Espere o próximo jogo começar.'
+          "Você não está no jogo! Espere o próximo jogo começar."
         );
       }
-      const [command, ...param] = message.trim().split(' '); // Remove os espaços extra e divide por " "
+      const [command, ...param] = message.trim().split(" "); // Remove os espaços extra e divide por " "
       // Esse parâmetro é opcional, então pode ser undefined. Ele pode ser um jogador alvo, ou o nome de uma carta (no caso de coupDrop)
       const playerAction = playerActions[command];
       const inTurnAction = inTurnActions[command];
@@ -155,9 +183,11 @@ socket.on('connect', () => {
         } else {
           renderSecretMessage(`O jogo ainda não começou.`);
         }
+        updateHistory(command);
       } else if (inTurnAction) {
         if (game.state.isStarted) {
           inTurnAction(param);
+          updateHistory(command);
         } else {
           renderSecretMessage(`O jogo ainda não começou.`);
         }
@@ -165,6 +195,7 @@ socket.on('connect', () => {
         const gameCommand = commands[command];
         if (gameCommand) {
           gameCommand();
+          updateHistory(command);
         } else {
           sendTextMessage(message);
         }
@@ -172,11 +203,11 @@ socket.on('connect', () => {
     } else {
       sendTextMessage(message);
     }
-    inputElement.value = '';
+    inputElement.value = "";
   });
 
   console.log(`${playerName} connected: ${myPlayerId}`);
-  socket.on('messageReceived', ({ player, message }) => {
+  socket.on("messageReceived", ({ player, message }) => {
     if (player.id === myPlayerId) {
       return;
     }
@@ -185,45 +216,42 @@ socket.on('connect', () => {
     console.log(message);
 
     // Verifica se a mensagem envolve pegar moedas e dispara a animação
-    if (
-      message.includes('pegou')
-    ) {
+    if (message.includes("pegou")) {
       renderer.showCoinAnimation();
-    } 
-    
-    if (message.includes('assassinado')) {
-      console.log('assassinado')
-      renderer.showMurderAnimation();
     }
 
+    if (message.includes("assassinado")) {
+      console.log("assassinado");
+      renderer.showMurderAnimation();
+    }
   });
 
-  socket.on('updateGame', (state) => {
+  socket.on("updateGame", (state) => {
     game.updateGame(state);
     if (!game.state.isStarted) {
       myCards = [];
     }
   });
 
-  socket.on('updateCards', ({ cards }) => {
-    console.log('Game started');
+  socket.on("updateCards", ({ cards }) => {
+    console.log("Game started");
     myCards = cards;
     renderer.renderReceivedMessage(
-      { name: 'JOGO' },
-      `Suas cartas: ${myCards.map((card) => card.name).join(' e ')}`
+      { name: "JOGO" },
+      `Suas cartas: ${myCards.map((card) => card.name).join(" e ")}`
     );
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`Player disconnected: ${myPlayerId}`);
     socket.close();
     renderer.renderReceivedMessage(
-      { name: 'JOGO' },
-      'Você foi desconectado. Recarregue a página para jogar novamente.'
-    )
+      { name: "JOGO" },
+      "Você foi desconectado. Recarregue a página para jogar novamente."
+    );
     inputElement.disabled = true;
-    const reloadButton = document.createElement('button');
-    reloadButton.textContent = 'Recarregar';
+    const reloadButton = document.createElement("button");
+    reloadButton.textContent = "Recarregar";
     reloadButton.onclick = () => window.location.reload();
     messagesElement.prepend(reloadButton);
   });
